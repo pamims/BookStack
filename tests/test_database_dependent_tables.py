ca31c2d4e08061fd6a3023df724a306f5ec6dee1
Powell Mims <powellamims@gmail.com>
2023-07-05 15:17:37 -0400

    Split project into smaller, more manageable files.

---
+++ tests/test_database_dependent_tables.py
@@ -0,0 +1,177 @@
+"""Test cases for tables that depend on referenced tables."""
+
+import sqlite3
+
+from tests.test_database_tables import BaseTableTestCase
+
+
+class BaseDependentTableTestCase(BaseTableTestCase):
+    """
+    Base class for test cases where multiple tables must exist prior to running
+    tests.
+    """
+    referenced_table_names: tuple[str] = ()
+
+    def __init__(self, methodName: str = "runTest"):
+        """
+        Gets the necessary create_table_* and insert_* functions to set up the
+        test environment.
+        """
+        super().__init__(methodName)
+        self.insert_funcs = tuple(
+            self.get_table_function_from_dictionary(
+                name, self.db_insert_functions
+            ) for name in self.referenced_table_names
+        )
+        self.create_table_funcs = tuple(
+            self.get_table_function_from_dictionary(
+                name, self.db_create_table_functions
+            ) for name in self.referenced_table_names
+        )
+
+    def setUp(self):
+        """Creates and populates the necessary tables."""
+        # Create the test table.
+        super().setUp()
+        for create_table_func in self.create_table_funcs:
+            create_table_func(self.db_path)
+        # Make 5 insertions in each table.
+        for i in range(1, 6):
+            for table_name, insert_func in zip(
+                self.referenced_table_names, self.insert_funcs
+            ):
+                # Need to know the column names to generate input.
+                column_names = self.get_table_column_names(table_name)
+                params = (
+                    i if 'ID' in column_name else column_name + str(i)
+                    for column_name in column_names[1:]  # don't want ID column
+                )
+                try:
+                    insert_func(self.db_path, *params)
+                except sqlite3.IntegrityError as exc:
+                    err_msg = str(exc)
+                    if "FOREIGN KEY constraint failed" in err_msg:
+                        self.fail(
+                            "Check the order that table names are defined in "
+                            "the referenced_table_names tuple. Referenced "
+                            "tables must be listed before dependent tables."
+                        )
+                    else:
+                        self.fail(str(exc))
+                except sqlite3.Error as exc:
+                    self.fail(str(exc))
+
+
+class TitleAuthorTableTestCase(BaseDependentTableTestCase):
+    """
+    Tests for validating TitleAuthor table function. TitleAuthors must be
+    inserted correctly with auto-incrementing ID's. Each (TitleID, AuthorID)
+    combination must be unique, and they must have FOREIGN KEY constraints.
+    """
+    table_name = 'TitleAuthor'
+    referenced_table_names = ('Title', 'Author')
+
+    def test_titleauthor_insert_record_creation(self) -> None:
+        """Verifies insert_titleauthor() creates a valid record."""
+        title_ids = self.get_valid_record_id('Title')
+        author_ids = self.get_valid_record_id('Author')
+        author_ids = author_ids[-1:] + author_ids[:-1]
+        params_list = tuple(zip(title_ids, author_ids))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_list
+        )
+
+    def test_titleauthor_authorid_titleid_unique_constraint(self) -> None:
+        """Verifies the unique constraint of TitleID, AuthorID pairs."""
+        title_ids = self.get_valid_record_id('Title')
+        author_ids = self.get_valid_record_id('Author')
+        params_list = next(
+            ((t, a), ) * 2 for t in title_ids for a in author_ids if t != a
+        )
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_list, 2
+        )
+
+    def test_titleauthor_authorid_foreign_key_constraint(self) -> None:
+        """Verifies the foreign key constraint of authorid."""
+        title_ids = self.get_valid_record_id('Title')
+        author_ids = self.get_valid_record_id('Author')
+        ids = set(title_ids).union(author_ids)
+        invalid_id = next(
+            i for i in range(1, len(ids) + 2) if i not in ids
+        )
+        params_list = ((title_ids[0], invalid_id), )
+        self.assert_foreign_key_constraints(
+            self.table_name, self.insert_function, params_list
+        )
+
+    def test_titleauthor_titleid_foreign_key_constraint(self) -> None:
+        """Verifies the foreign key constraint of titleid."""
+        title_ids = self.get_valid_record_id('Title')
+        author_ids = self.get_valid_record_id('Author')
+        ids = set(title_ids).union(author_ids)
+        invalid_id = next(
+            i for i in range(1, len(ids) + 2) if i not in ids
+        )
+        params_list = ((invalid_id, author_ids[0]), )
+        self.assert_foreign_key_constraints(
+            self.table_name, self.insert_function, params_list
+        )
+
+
+class WorkTableTestCase(BaseDependentTableTestCase):
+    """
+    Tests for validating Work table function. Work must be inserted correctly
+    with auto-incrementing ID's. Each (TitleAuthorID, GenreID) combination must
+    be unique, and they must have FOREIGN KEY constraints.
+    """
+
+    table_name = 'Work'
+    referenced_table_names = ('Title', 'Author', 'TitleAuthor', 'Genre')
+
+    def test_work_insert_record_createion(self) -> None:
+        """Verifies insert_work() creates a valid record."""
+        ta_ids = self.get_valid_record_id('TitleAuthor')
+        genre_ids = self.get_valid_record_id('Genre')
+        genre_ids = genre_ids[-1:] + genre_ids[:-1]
+        params_list = tuple(zip(ta_ids, genre_ids))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_list
+        )
+
+    def test_work_titleauthorid_genreid_unique_constraint(self) -> None:
+        """Verifies the unique constraint of TitleAuthorID, GenreID pairs."""
+        ta_ids = self.get_valid_record_id('TitleAuthor')
+        genre_ids = self.get_valid_record_id('Genre')
+        params_list = next(
+            ((ta, g), ) * 2 for ta in ta_ids for g in genre_ids if ta != g
+        )
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_list, 2
+        )
+
+    def test_work_genreid_foreign_key_constraint(self) -> None:
+        """Verifies the foreign key constraint of genreid."""
+        ta_ids = self.get_valid_record_id('TitleAuthor')
+        genre_ids = self.get_valid_record_id('Genre')
+        ids = set(ta_ids).union(genre_ids)
+        invalid_id = next(
+            i for i in range(1, len(ids) + 2) if i not in ids
+        )
+        params_list = ((ta_ids[0], invalid_id), )
+        self.assert_foreign_key_constraints(
+            self.table_name, self.insert_function, params_list
+        )
+
+    def test_work_titleauthorid_foreign_key_constraint(self) -> None:
+        """Verifies the foreign key constraint of titleauthorid."""
+        ta_ids = self.get_valid_record_id('TitleAuthor')
+        genre_ids = self.get_valid_record_id('Genre')
+        ids = set(ta_ids).union(genre_ids)
+        invalid_id = next(
+            i for i in range(1, len(ids) + 2) if i not in ids
+        )
+        params_list = ((invalid_id, genre_ids[0]), )
+        self.assert_foreign_key_constraints(
+            self.table_name, self.insert_function, params_list
+        )
---
+++ tests/test_database_tables.py
@@ -0,0 +1,318 @@
+"""Test cases for tables that don't depend on referenced tables"""
+
+import inspect
+from typing import Any, Callable
+
+from source import database
+from tests.test_database import BaseDatabaseModuleTestCase
+
+
+class BaseTableTestCase(BaseDatabaseModuleTestCase):
+    """
+    Base class for test cases where a single table must exist before the test
+    can be performed. This class allows all child class tests to be updated
+    from a single location. Ex. if an insertion function is renamed, it only
+    needs to be renamed in the db_insert_functions dictionary.
+
+    __init__, self.insert_function --
+    The __init__ function detects which insert function the child class needs
+    by looking at the value of table_name and retrieving the function from the
+    db_insert_functions dictionary. The value is then stored in
+    self.insert_function.
+
+    setUp --
+    The setUp function similarly decides which create_table_* function to call
+    at the beginning of each test.
+    """
+    db_insert_functions = {
+        'Title': database.insert_title,
+        'Author': database.insert_author,
+        'Genre': database.insert_genre,
+        'TitleAuthor': database.insert_titleauthor,
+        'Work': database.insert_work,
+
+        'Format': database.insert_format,
+        'Publisher': database.insert_publisher,
+        'Publication': None,
+
+        'Condition': database.insert_condition,
+        'Location': None,
+        'Book': None
+    }
+    db_create_table_functions = {
+        'Title': database.create_table_title,
+        'Author': database.create_table_author,
+        'Genre': database.create_table_genre,
+        'TitleAuthor': database.create_table_titleauthor,
+        'Work': database.create_table_work,
+
+        'Format': database.create_table_format,
+        'Publisher': database.create_table_publisher,
+        'Publication': None,
+
+        'Condition': database.create_table_condition,
+        'Location': None,
+        'Book': None
+    }
+    table_name = None
+
+    def __init__(self, methodName: str = "runTest"):
+        """
+        Sets the create_table_function to be called during setUp. Function will
+        be a failure function if a problem occurs with retrieving the function.
+        The function is based on the defined table_name and the
+        create_table_functions dictionary.
+        """
+        super().__init__(methodName)
+        self.insert_function = self.get_table_function_from_dictionary(
+            self.table_name, self.db_insert_functions
+        )
+
+    def setUp(self) -> None:
+        """Create necessary table for testing based on table_name."""
+        create_table = self.get_table_function_from_dictionary(
+            self.table_name, self.db_create_table_functions
+        )
+        create_table(self.db_path)
+
+    def get_table_function_from_dictionary(
+            self: BaseDatabaseModuleTestCase, table_name: str,
+            dictionary: dict[str, Callable[[str, tuple[Any, ...]], Any]]
+    ) -> Callable[[str, tuple[Any, ...]], Any]:
+        """Retrieves function from a db_ dictionary based on table_name"""
+        self.validate_table_name(
+            table_name, dictionary,
+            f"Cannot locate function. Table '{table_name}' is not defined "
+            f"in the provided dictionary. Available keys are: "
+            f"{', '.join(dictionary)}"
+        )
+        func: Callable[[str, tuple[Any, ...]], Any] = (
+            dictionary[table_name]
+        )
+        if not (inspect.isfunction(func) or inspect.ismethod(func)):
+            if func is None:
+                msg = (
+                    f"'{table_name}' table function has not been added to the "
+                    f"provided dictionary. Verify function exists and add it "
+                    f"to the appropriate dictionary."
+                )
+            else:
+                msg = (
+                    f"'{table_name}' table function is not a function or "
+                    f"method. Verify correct function definition."
+                )
+            self.fail(msg)
+        return func
+
+
+class TitleTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Title table function. Titles must be inserted
+    correctly with correct auto-incrementing ID's. Title name should be
+    NOT NULL, and it should NOT have UNIQUE constraint.
+    """
+    table_name = 'Title'
+
+    def test_title_insert_record_creation(self) -> None:
+        """Verifies insert_publisher() creates a valid record."""
+        params_tuple = ((f'Name{i}', ) for i in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_title_name_not_unique_constraint(self) -> None:
+        """Verifies no unique constraint on title Name field."""
+        params_tuple = (('Name', ) for _ in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_title_name_not_null_constraint(self) -> None:
+        """Verifies not null constraint on title Name field."""
+        params_tuple = ((None, ), )
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+
+class GenreTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Genre table function. Genres must be inserted
+    correctly with correct auto-incrementing ID's. Title name should
+    be UNIQUE and NOT NULL.
+    """
+    table_name = 'Genre'
+
+    def test_genre_insert_record_creation(self) -> None:
+        """Verifies insert_genre() creates a valid record."""
+        params_tuple = ((f'Name{i}', ) for i in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_genre_name_unique_constraint(self) -> None:
+        """Verifies unique constraint on the genre name field."""
+        params_tuple = (('Name', ), ('Name', ))
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_genre_name_not_null_constraint(self) -> None:
+        """Verifies not null constraint on genre name field."""
+        params_tuple = ((None, ), )
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+
+class FormatTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Format table function. Formats must be inserted
+    correctly with correct auto-incrementing ID's. Format name should
+    be UNIQUE and NOT NULL.
+    """
+    table_name = 'Format'
+
+    def test_format_insert_record_creation(self) -> None:
+        """Verifies insert_format() creates a valid record."""
+        params_tuple = ((f'Name{i}', ) for i in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_format_name_unique_constraint(self) -> None:
+        """Verifies unique constraint on the format name field."""
+        params_tuple = (('Name', ), ('Name', ))
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_format_name_not_null_constraint(self) -> None:
+        """Verifies not null constraint on format name field."""
+        params_tuple = ((None, ), )
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+
+class PublisherTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Publisher table function. Publishers must be inserted
+    correctly with correct auto-incrementing ID's. Publisher name should
+    be UNIQUE and NOT NULL.
+    """
+    table_name = 'Publisher'
+
+    def test_publisher_insert_record_creation(self) -> None:
+        """Verifies insert_publisher() creates a valid record."""
+        params_tuple = ((f'Name{i}', ) for i in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_publisher_name_unique_constraint(self) -> None:
+        """Verifies unique constraint on the publisher name field."""
+        params_tuple = (('Name', ), ('Name', ))
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_publisher_name_not_null_constraint(self) -> None:
+        """Verifies not null constraint on publisher name field."""
+        params_tuple = ((None, ), )
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+
+class ConditionTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Condition table function. Conditions must be inserted
+    correctly with auto-incrementing ID's. Condition names and descriptions
+    must be NOT NULL and UNIQUE.
+    """
+    table_name = 'Condition'
+
+    def test_condition_insert_record_function(self) -> None:
+        """Verifies insert_condition() creates a valid record."""
+        params_tuple = ((f'Name{i}', f'Description{i}') for i in range(1, 10))
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_condition_name_unique_constraint(self) -> None:
+        """Verifies unique constraint on condition name field."""
+        params_tuple = (('Name', 'Description1'), ('Name', 'Description2'))
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_condition_description_unique_constraint(self) -> None:
+        """Verifies unique constraint on condition description field."""
+        params_tuple = (('Name1', 'Description'), ('Name2', 'Description'))
+        self.assert_unique_table_constraint(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_condition_name_and_description_not_null_constraints(self) -> None:
+        """
+        Verifies not null constraints on condition name and description fields.
+        """
+        params_tuple = ((None, 'Description'), ('Name', None))
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+
+class AuthorTableTestCase(BaseTableTestCase):
+    """
+    Tests for validating Author table function. Authors must be inserted
+    correctly with auto-incrementing ID's. Author First[Name] must be NOT NULL.
+    Author Prefix, Middle, Last, and Suffix must be NULLABLE.
+    """
+    table_name = 'Author'
+
+    def test_author_insert_record_creation(self) -> None:
+        """Verifies insert_author() creates a valid record."""
+        params_tuple = (
+            (f'Prefix{i}', f'First{i}', f'Middle{i}', f'Last{i}', f'Suffix{i}')
+            for i in range(1, 10)
+        )
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_author_first_not_null_constraint(self) -> None:
+        """Verifies not null constraint on author first field."""
+        params_tuple = (('Prefix', None, 'Middle', 'Last', 'Suffix'), )
+        self.assert_not_null_table_constraints(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_author_prefix_nullable_constraint(self) -> None:
+        """Verifies author prefix field nullable."""
+        params_tuple = ((None, 'First', 'Middle', 'Last', 'Suffix'), )
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_author_middle_nullable_constraint(self) -> None:
+        """Verifies author middle field nullable."""
+        params_tuple = (('Prefix', 'First', None, 'Last', 'Suffix'), )
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_author_last_nullable_constraint(self) -> None:
+        """Verifies author last field nullable."""
+        params_tuple = (('Prefix', 'First', 'Middle', None, 'Suffix'), )
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
+
+    def test_author_suffix_nullable_constraint(self) -> None:
+        """Verifies author suffix field nullable."""
+        params_tuple = (('Prefix', 'First', 'Middle', 'Last', None), )
+        self.assert_record_insertion_correct(
+            self.table_name, self.insert_function, params_tuple
+        )
