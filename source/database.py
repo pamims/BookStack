caa02ca82609c91a7916b2763da6495f167a9ea9
Powell Mims <powellamims@gmail.com>
2023-07-06 22:02:14 -0400

    Alphabetize databese.py by table name.

--- source/database.py
+++ source/database.py
@@ -30,75 +30,11 @@ def db_connection(
     return wrapper
 
 
-# Table Creation Functions
+# Query Functions
 
 
-@db_connection
-def create_table_title(cursor: sqlite3.Cursor) -> None:
-    """Create title table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Title (
-            ID INTEGER PRIMARY KEY,
-            Name TEXT NOT NULL
-        )
-        '''
-    )
-
-
-@db_connection
-def create_table_genre(cursor: sqlite3.Cursor) -> None:
-    """Create genre table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Genre (
-            ID INTEGER PRIMARY KEY,
-            Name TEXT UNIQUE NOT NULL
-        )
-        '''
-    )
-
-
-@db_connection
-def create_table_format(cursor: sqlite3.Cursor) -> None:
-    """Create format table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Format (
-            ID INTEGER PRIMARY KEY,
-            Name TEXT UNIQUE NOT NULL
-        )
-        '''
-    )
-
-
-@db_connection
-def create_table_publisher(cursor: sqlite3.Cursor) -> None:
-    """Create publisher table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Publisher (
-            ID INTEGER PRIMARY KEY,
-            Name TEXT UNIQUE NOT NULL
-        )
-        '''
-    )
-
-
-@db_connection
-def create_table_condition(cursor: sqlite3.Cursor) -> None:
-    """Create condition table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Condition (
-            ID INTEGER PRIMARY KEY,
-            Name TEXT UNIQUE NOT NULL,
-            Description TEXT UNIQUE NOT NULL
-        )
-        '''
-    )
-
-
+# ## AUTHOR TABLE ## ##########################################################
+#
 @db_connection
 def create_table_author(cursor: sqlite3.Cursor) -> None:
     """Create author table query."""
@@ -117,39 +53,179 @@ def create_table_author(cursor: sqlite3.Cursor) -> None:
 
 
 @db_connection
-def create_table_titleauthor(cursor: sqlite3.Cursor) -> None:
-    """Create author table query."""
+def insert_author(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into author table."""
     cursor.execute(
         '''
-        CREATE TABLE TitleAuthor (
+        INSERT INTO Author (Prefix, First, Middle, Last, Suffix)
+        VALUES (?, ?, ?, ?, ?)
+        ''', args
+    )
+#
+# ## AUTHOR TABLE ## #
+
+
+# ## BOOK TABLE ## ############################################################
+#
+@db_connection
+def create_table_book(cursor: sqlite3.Cursor) -> None:
+    """Create book table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Book (
             ID INTEGER PRIMARY KEY,
-            TitleID INTEGER NOT NULL,
-            AuthorID INTEGER NOT NULL,
-            UNIQUE (TitleID, AuthorID),
-            FOREIGN KEY (TitleID) REFERENCES Title(ID),
-            FOREIGN KEY (AuthorID) REFERENCES Author(ID)
+            PublicationID INTEGER NOT NULL,
+            ConditionID INTEGER,
+            LocationID INTEGER,
+            FOREIGN KEY (PublicationID) REFERENCES Publication(ID),
+            FOREIGN KEY (ConditionID) REFERENCES Condition(ID),
+            FOREIGN KEY (LocationID) REFERENCES Location(ID)
         )
         '''
     )
 
 
 @db_connection
-def create_table_work(cursor: sqlite3.Cursor) -> None:
-    """Create work table query."""
+def insert_book(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into book table."""
     cursor.execute(
         '''
-        CREATE TABLE Work (
+        INSERT INTO Book (PublicationID, ConditionID, LocationID)
+        VALUES (?, ?, ?)
+        ''', args
+    )
+#
+# ## BOOK TABLE ## #
+
+
+# ## CONDITION TABLE ## #######################################################
+#
+@db_connection
+def create_table_condition(cursor: sqlite3.Cursor) -> None:
+    """Create condition table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Condition (
             ID INTEGER PRIMARY KEY,
-            TitleAuthorID INTEGER NOT NULL,
-            GenreID INTEGER,
-            UNIQUE (TitleAuthorID, GenreID),
-            FOREIGN KEY (TitleAuthorID) REFERENCES TitleAuthor(ID),
-            FOREIGN KEY (GenreID) REFERENCES Genre(ID)
+            Name TEXT UNIQUE NOT NULL,
+            Description TEXT UNIQUE NOT NULL
         )
         '''
     )
 
 
+@db_connection
+def insert_condition(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into condition table."""
+    cursor.execute(
+        '''
+        INSERT INTO Condition (Name, Description)
+        VALUES (?, ?)
+        ''', args
+    )
+#
+# ## CONDITION TABLE ## #
+
+
+# ## FORMAT TABLE ## ##########################################################
+#
+@db_connection
+def create_table_format(cursor: sqlite3.Cursor) -> None:
+    """Create format table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Format (
+            ID INTEGER PRIMARY KEY,
+            Name TEXT UNIQUE NOT NULL
+        )
+        '''
+    )
+
+
+@db_connection
+def insert_format(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into format table."""
+    cursor.execute(
+        '''
+        INSERT INTO Format (Name)
+        VALUES (?)
+        ''', args
+    )
+#
+# ## FORMAT TABLE ## #
+
+
+# ## GENRE TABLE ## ###########################################################
+#
+@db_connection
+def create_table_genre(cursor: sqlite3.Cursor) -> None:
+    """Create genre table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Genre (
+            ID INTEGER PRIMARY KEY,
+            Name TEXT UNIQUE NOT NULL
+        )
+        '''
+    )
+
+
+@db_connection
+def insert_genre(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into genre table."""
+    cursor.execute(
+        '''
+        INSERT INTO Genre (Name)
+        VALUES (?)
+        ''', args
+    )
+#
+# ## GENRE TABLE ## #
+
+
+# ## LOCATION TABLE ## ########################################################
+#
+@db_connection
+def create_table_location(cursor: sqlite3.Cursor) -> None:
+    """Create location table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Location (
+            ID INTEGER PRIMARY KEY,
+            Name TEXT UNIQUE NOT NULL,
+            Description TEXT
+        )
+        '''
+    )
+
+
+@db_connection
+def insert_location(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into condition table."""
+    cursor.execute(
+        '''
+        INSERT INTO Location (Name, Description)
+        VALUES (?, ?)
+        ''', args
+    )
+#
+# ## LOCATION TABLE ## #
+
+
+# ## PUBLICATION TABLE ## #####################################################
+#
 @db_connection
 def create_table_publication(cursor: sqlite3.Cursor) -> None:
     """Create publication table query."""
@@ -171,76 +247,32 @@ def create_table_publication(cursor: sqlite3.Cursor) -> None:
 
 
 @db_connection
-def create_table_location(cursor: sqlite3.Cursor) -> None:
-    """Create location table query."""
+def insert_publication(
+    cursor: sqlite3.Cursor, *args: Union[str, int, float]
+) -> None:
+    """Insert record into publication table."""
     cursor.execute(
         '''
-        CREATE TABLE Location (
+        INSERT INTO Publication (WorkID, FormatID, PublisherID, ISBN)
+        VALUES (?, ?, ?, ?)
+        ''', args
+    )
+#
+# ## PUBLICATION TABLE ## #
+
+
+# ## PUBLISHER TABLE ## #######################################################
+#
+@db_connection
+def create_table_publisher(cursor: sqlite3.Cursor) -> None:
+    """Create publisher table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Publisher (
             ID INTEGER PRIMARY KEY,
-            Name TEXT UNIQUE NOT NULL,
-            Description TEXT
+            Name TEXT UNIQUE NOT NULL
         )
         '''
-    )
-
-
-@db_connection
-def create_table_book(cursor: sqlite3.Cursor) -> None:
-    """Create book table query."""
-    cursor.execute(
-        '''
-        CREATE TABLE Book (
-            ID INTEGER PRIMARY KEY,
-            PublicationID INTEGER NOT NULL,
-            ConditionID INTEGER,
-            LocationID INTEGER,
-            FOREIGN KEY (PublicationID) REFERENCES Publication(ID),
-            FOREIGN KEY (ConditionID) REFERENCES Condition(ID),
-            FOREIGN KEY (LocationID) REFERENCES Location(ID)
-        )
-        '''
-    )
-
-
-# Insertion Functions
-
-
-@db_connection
-def insert_title(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into title table."""
-    cursor.execute(
-        '''
-        INSERT INTO Title (Name)
-        VALUES (?)
-        ''', args
-    )
-
-
-@db_connection
-def insert_genre(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into genre table."""
-    cursor.execute(
-        '''
-        INSERT INTO Genre (Name)
-        VALUES (?)
-        ''', args
-    )
-
-
-@db_connection
-def insert_format(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into format table."""
-    cursor.execute(
-        '''
-        INSERT INTO Format (Name)
-        VALUES (?)
-        ''', args
     )
 
 
@@ -255,31 +287,56 @@ def insert_publisher(
         VALUES (?)
         ''', args
     )
+#
+# ## PUBLISHER TABLE ## #
 
 
+# ## TITLE TABLE ## ###########################################################
+#
 @db_connection
-def insert_condition(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into condition table."""
+def create_table_title(cursor: sqlite3.Cursor) -> None:
+    """Create title table query."""
     cursor.execute(
         '''
-        INSERT INTO Condition (Name, Description)
-        VALUES (?, ?)
-        ''', args
+        CREATE TABLE Title (
+            ID INTEGER PRIMARY KEY,
+            Name TEXT NOT NULL
+        )
+        '''
     )
 
 
 @db_connection
-def insert_author(
+def insert_title(
     cursor: sqlite3.Cursor, *args: Union[str, int, float]
 ) -> None:
-    """Insert record into author table."""
+    """Insert record into title table."""
     cursor.execute(
         '''
-        INSERT INTO Author (Prefix, First, Middle, Last, Suffix)
-        VALUES (?, ?, ?, ?, ?)
+        INSERT INTO Title (Name)
+        VALUES (?)
         ''', args
+    )
+#
+# ## TITLE TABLE ## #
+
+
+# ## TITLEAUTHOR TABLE ## #####################################################
+#
+@db_connection
+def create_table_titleauthor(cursor: sqlite3.Cursor) -> None:
+    """Create author table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE TitleAuthor (
+            ID INTEGER PRIMARY KEY,
+            TitleID INTEGER NOT NULL,
+            AuthorID INTEGER NOT NULL,
+            UNIQUE (TitleID, AuthorID),
+            FOREIGN KEY (TitleID) REFERENCES Title(ID),
+            FOREIGN KEY (AuthorID) REFERENCES Author(ID)
+        )
+        '''
     )
 
 
@@ -294,6 +351,27 @@ def insert_titleauthor(
         VALUES (?, ?)
         ''', args
     )
+#
+# ## TITLEAUTHOR TABLE ## #
+
+
+# ## WORK TABLE ## ############################################################
+#
+@db_connection
+def create_table_work(cursor: sqlite3.Cursor) -> None:
+    """Create work table query."""
+    cursor.execute(
+        '''
+        CREATE TABLE Work (
+            ID INTEGER PRIMARY KEY,
+            TitleAuthorID INTEGER NOT NULL,
+            GenreID INTEGER,
+            UNIQUE (TitleAuthorID, GenreID),
+            FOREIGN KEY (TitleAuthorID) REFERENCES TitleAuthor(ID),
+            FOREIGN KEY (GenreID) REFERENCES Genre(ID)
+        )
+        '''
+    )
 
 
 @db_connection
@@ -307,42 +385,5 @@ def insert_work(
         VALUES (?, ?)
         ''', args
     )
-
-
-@db_connection
-def insert_publication(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into publication table."""
-    cursor.execute(
-        '''
-        INSERT INTO Publication (WorkID, FormatID, PublisherID, ISBN)
-        VALUES (?, ?, ?, ?)
-        ''', args
-    )
-
-
-@db_connection
-def insert_location(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into condition table."""
-    cursor.execute(
-        '''
-        INSERT INTO Location (Name, Description)
-        VALUES (?, ?)
-        ''', args
-    )
-
-
-@db_connection
-def insert_book(
-    cursor: sqlite3.Cursor, *args: Union[str, int, float]
-) -> None:
-    """Insert record into book table."""
-    cursor.execute(
-        '''
-        INSERT INTO Book (PublicationID, ConditionID, LocationID)
-        VALUES (?, ?, ?)
-        ''', args
-    )
+#
+# ## WORK TABLE ## #
